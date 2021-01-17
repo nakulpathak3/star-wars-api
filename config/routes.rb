3b35606f43135604fef602a0d8f0611563ab949a
Nakul Pathak <nakulp@scribd.com>
2021-01-17 12:20:43 -0500

    Add API::People controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get 'tic_tac_toe', to: 'tic_tac_toe#index'
+
+  namespace :api do
+    get 'people', to: 'people#index'
+    get 'people/:id', to: 'people#get'
+    get 'people/search/:query', to: 'people#search'
+  end
 end
